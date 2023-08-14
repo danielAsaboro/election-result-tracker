ec45c8fa3a82c0179f94f6d55d880ead94fdef80
lekkss <leksguy9@gmail.com>
2023-08-14 21:54:07 +0100

    update route

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Election Result Tracker Api");
+});
+
 // ?route
 app.use("/api/v1/auth", authRouter);
 
